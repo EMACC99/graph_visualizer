2c8e99808882962c85242a1e84b64026e47619f8
emacc <lalitoceja@gmail.com>
2021-04-02 13:05:50 -0600

    deleted comented code

--- includes/edge.hpp
+++ includes/edge.hpp
@@ -8,14 +8,10 @@ struct edge{
         std::vector<int> nodes; //de mi lista de verices, cuales conecto con esta arista
         bool colored = false;
 
-        // edge(float peso, std::vector<node> nodes);
-}; 
+};
 struct compare{  //para ordenarlos por peso en la queue
     bool operator()(const edge & a, const edge & b){
         return a.peso > b.peso;
     }
 };
-// edge::edge(float peso, std::vector<node> nodes){
-//     peso = peso;
-//     nodes = nodes;
-// }   
+
--- includes/node.hpp
+++ includes/node.hpp
@@ -6,12 +6,5 @@ struct node{
         int root_id;
         int id;
         float x,y;
-        // std::vector<int> vecinos;
         std::vector <int> arista_index; // de la lista de aristas, pon los que le corresponden, moejor dicho, todas la aristas que salen del vertice
 };
-
-// node::node(int node_id, const sf::Vector2f &position){
-//     id = node_id;
-//     x = position.x;
-//     y = position.y;
-// } 
