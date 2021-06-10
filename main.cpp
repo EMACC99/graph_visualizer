621ff01914e5978799e358a446dff367308acfbb
emacc <lalitoceja@gmail.com>
2021-06-10 11:06:08 -0500

    added arg parser with argh

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include "argh/argh.h"
 
 #include "includes/globals.hpp"
 #include "includes/visualizer.hpp"
@@ -7,15 +8,14 @@
 
 int main(int argc, char *argv[]){
     srand(time(NULL)); //semilla pal random
-// cosas para el unit test
-    if (argc == 2){
-        std::string arg = argv[1];
-        if (arg == "test"){
-            test();
-            // return 0; //corre los test y finaliza el programa
-            is_test = true;
-        }   
+    
+    argh::parser cmdl(argv);
+    
+    if (cmdl[{"-t", "--test"}]){
+    test();
+    is_test = true;    
     }
+    
     else if (argc > 2){ //si me pasas muchos argumentos o uno que no es, continua con el programa
         std::cout << "muchos argumentos, solo quiero uno >:(" << std::endl;
     }
