c9bbb4a5359d5e42df27c7c48f8411a909a269c8
emacc <lalitoceja@gmail.com>
2021-06-12 18:33:56 -0500

    fixed test not showing

--- main.cpp
+++ main.cpp
@@ -10,15 +10,10 @@ int main(int argc, char *argv[]){
     srand(time(NULL)); //semilla pal random
 
     argh::parser cmdl(argv);
-    if (cmdl.size() > 1){
-        if (cmdl[{"-t", "--test"}]){
+    if (cmdl[{"-t", "--test"}]){
             test();
             is_test = true;    
         }
-        else { //si me pasas muchos argumentos o uno que no es, continua con el programa
-            std::cout << "Argumentos no aceptados" << std::endl;
-        }
-    }
 
     //cargar las texturas
     if (!blue_circle.loadFromFile("Assets/blue-circle.png") || !red_circle.loadFromFile("Assets/red-circle.png")){
