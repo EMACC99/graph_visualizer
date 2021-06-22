68a3dde1e502e4670699a7cef1ecc1254d3fe6d6
emacc <lalitoceja@gmail.com>
2021-06-22 17:45:46 -0500

    added height parameter to node

--- includes/node.hpp
+++ includes/node.hpp
@@ -3,8 +3,9 @@
 
 struct node{
     int padre;
-    int id;
+    unsigned int id;
     float x,y;
+    int heigth = -1; // the height of the node (only applies to trees)
     std::vector <int> arista_index; // de la lista de aristas, pon los que le corresponden, moejor dicho, todas la aristas que salen del vertice
 };
 
