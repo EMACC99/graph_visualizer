174613fa14d1cf0682d95c9aee55a916f55e8b09
emacc99 <lalitoceja@gmail.com>
2021-08-31 23:05:05 -0500

    more base work for the matchings

--- algorithms/apareamientos.hpp
+++ algorithms/apareamientos.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <list>
+
 #include "../includes/globals.hpp"
 #include "../includes/edge.hpp"
 #include "../includes/node.hpp"
@@ -23,12 +25,21 @@ std::vector<int> apareamientos_iniciales(){
 }
 
 
-void bsf(){
-    
-}
+void bsf(const node &root){
+    std::vector<bool> visited(vertices.size(), false);
+    std::vector<int> queue;
+
+    visited[root.id] = true;
+
+    queue.push_back(root.id);
+
+    std::vector<int> vecinos = get_vecinos(root);
+
+
+}   
 
 std::vector<node> apareamientos(){
     // primero, tenemos que hacer los apareamientos iniciales
     std::vector<int> inital_matches = apareamientos_iniciales();
-
+    
 }
